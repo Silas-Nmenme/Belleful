6a236a5fb65ff84239b22b0d120bf98f61727235
Your Name <you@example.com>
2026-04-01 13:40:56 +0100

    `Update server.js to allow PATCH method in CORS configuration`

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use(helmet({
 app.use(cors({
   origin: [FRONTEND_URL, 'https://bellefulchop.netlify.app'],
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
